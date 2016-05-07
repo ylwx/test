220b03a21c40f7d19f55282b57e64e9c2e71207a
ytx <ytx@163.com>
2016-05-07 22:11:33 +0800

    test

---
+++ WebApplication3/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Salary { get; set; }
+    }
+}
---
+++ WebApplication3/ViewModels/EmployeeBusinessLayer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication3.Models;
+
+namespace WebApplication3.ViewModels
+{
+    public class EmployeeBusinessLayer
+    {
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> Employees = new List<Employee>();
+            Employee emp = new Employee();
+
+            emp.FirstName = "johnson";
+
+            emp.LastName = " fernandes";
+            emp.Salary = 14000;
+
+            Employees.Add(emp);
+
+            emp = new Employee();
+
+            emp.FirstName = "michael";
+
+            emp.LastName = "jackson";
+
+            emp.Salary = 16000;
+
+            Employees.Add(emp);
+
+            emp = new Employee();
+
+            emp.FirstName = "robert";
+
+            emp.LastName = " pattinson";
+
+            emp.Salary = 20000;
+
+            Employees.Add(emp);
+
+            return Employees;
+        }
+    }
+}
